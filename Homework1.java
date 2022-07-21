8bcee4a8207960ba20bbf53b227d21c8e4ae23b6
unknown <lpatzanl2@miumg.edu.gt>
2022-07-20 20:38:48 -0600

    Add examples files VHDL

---
+++ Homework1.java
@@ -0,0 +1,11 @@
+package TareaUno;
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String [] args){
+        Scanner pudin = new Scanner(System.in);
+        System.out.println("Ingresar su nombre");
+        String nombre = pudin.nextLine();
+        System.out.println("\nBienvenido a progra 2, 2022 " + nombre );
+        }
+}
